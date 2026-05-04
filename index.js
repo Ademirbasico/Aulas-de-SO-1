c43d499eaff2d2cce1e3f1fec6f455e2debc9eaa
ademir <maninhoo111@gmail.com>
2026-05-04 09:46:49 -0300

    ,,

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
     <h1>Monitor de Sistemas Operacionais</h1>
-    <p><strong>Hostname:</strong> ${os.hostname()}</p>
-    <p><strong>Plataforma:</strong> ${os.platform()}</p>
-    <p><strong>Arquitetura:</strong> ${os.arch()}</p>
-    <p><strong>Memória Total:</strong> ${Math.round(os.totalmem()/1024/1024)} MB</p>
-    <p><strong>Memória Livre:</strong> ${Math.round(os.freemem()/1024/1024)} MB</p>
-    <p><strong>CPUs:</strong> ${os.cpus().length}</p>
-    <p><strong>Uptime:</strong> ${Math.round(os.uptime()/60)} minutos</p>
+    <table border="1" cellpadding="8" cellspacing="0">
+      <tr><td><p><strong>Hostname:</strong> ${os.hostname()}</p></td></tr>
+      <tr><td><p><strong>Plataforma:</strong> ${os.platform()}</p></td></tr>
+      <tr><td><p><strong>Arquitetura:</strong> ${os.arch()}</p></td></tr>
+      <tr><td><p><strong>Memória Total:</strong> ${Math.round(os.totalmem()/1024/1024)} MB</p></td></tr>
+      <tr><td><p><strong>Memória Livre:</strong> ${Math.round(os.freemem()/1024/1024)} MB</p></td></tr>
+      <tr><td><p><strong>CPUs:</strong> ${os.cpus().length}</p></td></tr>
+      <tr><td><p><strong>Uptime:</strong> ${Math.round(os.uptime()/60)} minutos</p></td></tr>
+    </table>
   `);
 });
 
